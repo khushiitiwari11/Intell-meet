21ff5ac83a0c68e254de6f15ce0eb316f91ed8a9
khushiitiwari11 <khushii.ti11@gmail.com>
2026-04-21 21:59:59 +0530

    new changes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,9 +8,14 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
 Sentry.init({
-  dsn: "YOUR_DSN_HERE",
-  integrations: [Sentry.browserTracingIntegration()], 
-  tracesSampleRate: 1.0,
+  dsn: "https://a7c0e7622c5e2b75e0f9b598423bee88@o4511258948075520.ingest.de.sentry.io/4511258949648464",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true
 });
+
+const container = document.getElementById("app");
+const root = createRoot(container);
+root.render(<App />);
